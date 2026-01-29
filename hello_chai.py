33e79ebf870109a2c0415a81f8db42765840cbae
Riya Saharan <riachoudhary59@gmail.com>
2026-01-29 22:53:11 +0530

    initial commit: python from scratch

---
+++ hello_chai.py
@@ -0,0 +1,32 @@
+print("Hello World")
+print("*" * 10)
+x = 3
+# shift+ctrl+p for >
+students_count = 1000
+rating = 4.99
+is_published = True
+course_name = "Python Programming"
+print(students_count)
+message = """
+Hi Ankit,
+I'm Riya Saharan from Sri Ganganagar 
+blah blah blahhh
+"""
+print(message)
+course = "Python Pro"
+print(len(course))
+# if you want to access to a specific character in the string use square bracket notation
+print(course[0])
+# -1 gives last character
+print(course[-1])
+# if you want 3 characters
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+# copy of the original string
+print(course[:])
+# backslash \ in python
+# called excape character used to escape character after
+coursee = "Python \"Programming"
+print(coursee)
+#backslash double quote \" is an escape sequence
