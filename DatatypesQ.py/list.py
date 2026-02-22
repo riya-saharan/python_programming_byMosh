4d7dd8c61de41719680f5c5a844b80af2ba3f311
Riya-Saharan <riachoudhary59@gmail.com>
2026-02-22 23:51:55 +0530

    python questions

--- DatatypesQ.py/list.py
+++ DatatypesQ.py/list.py
@@ -4,3 +4,40 @@ numbers = [10, 45, 2, 78, 34]
 largest = max(numbers)
 
 print("Largest:", largest)
+# reverse a number(No String)
+num = 12345
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+print(reverse)
+# Armstrong Number (No pow)
+num = 153
+original = num
+sum = 0
+
+while num > 0:
+    digit = num % 10
+    sum += digit * digit * digit
+    num //= 10
+
+if sum == original:
+    print("Armstrong")
+else:
+    print("Not Armstrong")
+# Second Largest (No Sorting)
+arr = [10, 20, 4, 45, 99]
+
+largest = second = -999999
+
+for num in arr:
+    if num > largest:
+        second = largest
+        largest = num
+    elif num > second and num != largest:
+        second = num
+
+print("Second Largest:", second)
